8ef89edddd1ca52ed4590496e1634c5bafe65c23
hadi499 <hadi.ygcl@gmail.com>
2024-09-24 07:25:54 +0700

    up

--- book/models.py
+++ book/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 class Book(models.Model):
   title = models.CharField(max_length=255) 
@@ -10,8 +11,8 @@ class Book(models.Model):
 class Page(models.Model):
   title= models.ForeignKey(Book, on_delete=models.CASCADE)
   image = models.ImageField(upload_to='images/', null=True, blank=True)
-  english = models.TextField()
-  indonesian = models.TextField()
+  english = RichTextField(blank=True, null=True)
+  indonesian = RichTextField(blank=True, null=True)
 
   def __str__(self):
     return self.english
